ef9353d4892e64914fad929495b9280813469943
Joaquin <joacosanchez20@gmail.com>
2022-05-07 10:15:31 -0300

    change the about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => {
                     "What's next?" and that's when programming first appeared in my life. My father, who is a 
                     programmer too, recommended me to learn programming and see if I liked it. I started at freeCodeCamp
                     and that was the beginning of something else. I rapidly fell in love and wanted to learn more and
-                    more every day about this awesome world of programming and web development. After 5 months of hard
+                    more every day about this awesome world of programming and web development. After several months of hard
                     work I learned all the tools that I needed to develop my projects listed down here and I still learning
                     new things to get better. Moving aside from my story of how I started at programming, I like to train, 
                     play tennis, read academic papers related to investing, and yes, you guessed it, mountains!
